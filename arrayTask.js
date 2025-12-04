329ea13fd20cdc7074da81616c8ad4e29581c987
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-04 12:41:16 +0530

    filter and reduce

--- arrayTask.js
+++ arrayTask.js
@@ -8,41 +8,39 @@ console.log(result);
 
 //2. Create a function stringOnly that accepts any number of arguments, but returns an array containing only the arguments that are strings.
 
-
 //3. Create a function squareNumbers that accepts numbers as arguments and returns an array of each number squared.
 function squareNumbers(...numbers) {
-  return numbers.map(num => num * num);
+  return numbers.map((num) => num * num);
 }
 
 const q3 = squareNumbers(2, 4, 6, 8);
 console.log(q3);
 
 //4. Create a function getEvenNumbers that accepts numbers as arguments and returns an array containing only even numbers.
-function getEvenNumbers(...numbers){
-    return numbers.filter(num=>num%2==0);
+function getEvenNumbers(...numbers) {
+  return numbers.filter((num) => num % 2 == 0);
 }
 const q4 = getEvenNumbers(1, 2, 3, 4, 5, 6, 7, 8);
 console.log(q4);
 
 //5. Create a function collectNames that accepts name arguments and returns an array of names in uppercase.
+let op;
 function collectNames(...names) {
-  return names.map(name => String(name).toUpperCase());
+  op = names.map((name) => String(name).toUpperCase());
 }
-const q5 = collectNames("alice", "Bob", "charlie");
-console.log(q5);
+collectNames("alice", "Bob", "charlie");
+console.log(op);
 
 //6. Create a function makeUser that takes 3 arguments: name, age, city.
 function makeUser(name, age, city) {
-  return [
-    { name: name, age: age, city: city }
-  ];
+  return [{ name: name, age: age, city: city }];
 }
 const user = makeUser("Sid", 22, "Delhi");
 console.log(user);
 
 //7. Create a function addTen that takes any number of arguments and returns an array where each number is increased by 10.
 function addTen(...numbers) {
-  return numbers.map(num => num + 10);
+  return numbers.map((num) => num + 10);
 }
 
 const q7 = addTen(5, 20, 30);
@@ -60,5 +58,17 @@ function createFruitArray(...fruits) {
 console.log(createFruitArray());
 console.log(createFruitArray("mango", "orange"));
 
+let a = [1,2,3,4,5,6,7,8,9];
 
+//filter: truthy values
+let b = a.filter((el, index)=>{
+    return index>4;
+});
+console.log(b);
 
+//reduce: return a single value
+let c = a.reduce((acc, el, index)=>{
+    console.log(acc,el);
+    return acc+el;
+}, 200);
+console.log(c);
