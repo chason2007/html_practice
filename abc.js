23e4365f0dec1b41c2cad5496ecca866d2ebf039
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-04 11:47:30 +0530

    array iteration

--- abc.js
+++ abc.js
@@ -3,30 +3,28 @@ function namedFunction(){
     console.log("Named fn.");
     console.log("Named fn.");
 }
-
 namedFunction();
 
 let funcExp = function(){
     console.log("Fn. expression");
 };
-
 funcExp();
 
 let arrow = () =>{
     console.log("Arrow fn.");
 };
-
 arrow();
 
 {()=>{
     console.log("Callback and IIFE fn.");
-}}();
+}};
 
 const add = (parameter1, parameter2) => {
     let num1 = parameter1 || 10;
     let num2 = parameter2 || 20;
     let sum = num1+num2;
     console.log("The sum is: ", sum);
+    console.log(`The sum of ${num1} and ${num2} is ${sum}`);
 }
 add(20, 50);
 
@@ -53,3 +51,23 @@ const div = (parameter1, parameter2) => {
     console.log("The product is: ", ans);
 }
 div(20, 50);
+
+let a;
+console.log(typeof(a))
+
+let arr = [1,2,3,4,5,6];
+
+//for loop
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
+
+//forEach
+arr.forEach((element, index)=>{
+    console.log(element, index);
+});
+
+//map
+arr.map((el, ind)=>{
+    console.log(el, ind);
+});
