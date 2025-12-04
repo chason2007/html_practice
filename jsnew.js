08fc30a34a8ec9a1ced03887aa12354eff63a29b
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-04 10:32:59 +0530

    types of functions

--- jsnew.js
+++ jsnew.js
@@ -21,3 +21,32 @@ console.log(b);
 console.log(c);
 const c = 40;
 console.log(c);
+
+//functions
+//1. Named functions:
+function NamedFunction(){
+    console.log("Named fn called");
+    console.log("Named fn called");
+    console.log("Named fn called");
+}
+
+NamedFunction();
+
+//2. Function expression
+let funcExp = function(){
+    console.log("Function expression called");
+};
+
+funcExp();
+
+//3. Arrow functions
+let arrow = () =>{
+    console.log("Arrow function called");
+};
+
+arrow();
+
+//4. Callback function and 5. IIFE (immediately invoked function expression)
+{()=>{
+    console.log("Callback and iife f called");
+}}();
