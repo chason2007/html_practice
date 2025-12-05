a26ed0cca92d0f82bd93108ba4f8fc2a512fd709
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-05 11:17:40 +0530

    check button functionality

--- numberGuessing.js
+++ numberGuessing.js
@@ -1,5 +1,34 @@
-function refresh() {
-  window.location.reload();
-}
-const re = document.getElementById("again");
-re.addEventListener("click", handleClick);
+//Instances
+let againBtn = document.querySelector(".btn.again")
+let number = document.querySelector(".number")
+let guess = document.querySelector(".guess")
+let checkBtn = document.querySelector(".btn.check")
+let score = document.querySelector(".score")
+let highScore = document.querySelector(".highscore")
+let msg = document.querySelector(".message")
+
+//Generating random num
+let randomNumber = Math.trunc(Math.random()*20)+1;
+
+//create a variable score
+let s = 20;
+
+let inputVal
+//button functionalities
+checkBtn.addEventListener("click", () => {
+  inputVal = Number(guess.value);
+  console.log(randomNumber);
+  
+  if(inputVal === randomNumber){
+    console.log("Value correct!");
+  }
+  else if(inputVal > randomNumber){
+    console.log("Too high!");
+  }
+  else if(inputVal < randomNumber){
+    console.log("Too low!");
+  }
+  else{
+    console.log("No input value!");
+  }
+})  //parameters: event, callback fn.
