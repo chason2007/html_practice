1c1794aa99456f35ce998aeeca2bc6237f7258fd
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-05 09:32:19 +0530

    async

--- dom.js
+++ dom.js
@@ -4,17 +4,17 @@ console.log(tag[0]) //returns htmlCollection array
 
 //class selector
 let classs = document.getElementsByClassName("a");
-console.log(cl[0]) //return htmlCollection array
+console.log(classs[0]) //return htmlCollection array
 
 //id selector
 let id = document.getElementById("name")
 console.log(id); //returns first instance of given id
 
 //query selector (works on all 3 selectors)
-let query = document.querySelector("#b");
+let query = document.querySelector(".b");
 console.log(query) //returns first instance of given query
 
 //query selector all
-let allquery = document.querySelectorAll("#b")
+let allquery = document.querySelectorAll(".b")
 console.log(allquery) //returns nodeList array
 
