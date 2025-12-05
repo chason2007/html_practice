bc8dbcf06ca8d94df21dd7e33f001e6a8e2d8018
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-05 09:17:22 +0530

    dom selectors

---
+++ dom.js
@@ -0,0 +1,20 @@
+//Tag selector
+let tag = document.getElementsByTagName("h1")
+console.log(tag[0]) //returns htmlCollection array
+
+//class selector
+let classs = document.getElementsByClassName("a");
+console.log(cl[0]) //return htmlCollection array
+
+//id selector
+let id = document.getElementById("name")
+console.log(id); //returns first instance of given id
+
+//query selector (works on all 3 selectors)
+let query = document.querySelector("#b");
+console.log(query) //returns first instance of given query
+
+//query selector all
+let allquery = document.querySelectorAll("#b")
+console.log(allquery) //returns nodeList array
+
