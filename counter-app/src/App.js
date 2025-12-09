bdcfe59aea6ab258c5b19c00235ae7677d1ae5b1
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-09 09:47:27 +0530

    created named components

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -4,6 +4,7 @@ import Comp2 from "./Comp2";
 import Comp3 from "./Comp3";
 import Comp4 from "./Comp4";
 import Comp5 from "./Comp5";
+import { NamedComp1, NamedComp2, NamedComp3, NamedComp4, NamedComp5 } from "./NamedComps";
 
 function App() {
   return (
@@ -14,6 +15,11 @@ function App() {
       <Comp3></Comp3>
       <Comp4></Comp4>
       <Comp5></Comp5>
+      <NamedComp1></NamedComp1>
+      <NamedComp2></NamedComp2>
+      <NamedComp3></NamedComp3>
+      <NamedComp4></NamedComp4>
+      <NamedComp5></NamedComp5>
     </div>
 
   );
---
+++ counter-app/src/NamedComps.js
@@ -0,0 +1,39 @@
+export const NamedComp1 = () => {
+    return (
+        <div>
+            <h3>This is Named Component 1</h3>
+        </div>
+    )
+}
+
+export const NamedComp2 = () => {
+    return (
+        <div>
+            <h3>This is Named Component 2</h3>
+        </div>
+    )
+}
+
+export const NamedComp3 = () => {
+    return (
+        <div>
+            <h3>This is Named Component 3</h3>
+        </div>
+    )
+}
+
+export const NamedComp4 = () => {
+    return (
+        <div>
+            <h3>This is Named Component 4</h3>
+        </div>
+    )
+}
+
+export const NamedComp5 = () => {
+    return (
+        <div>
+            <h3>This is Named Component 5</h3>
+        </div>
+    )
+}
