a29e15b4fa93b7aacfb3a6d56c75aedac2b69b3a
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-10 12:27:45 +0530

    add alt attribute to logo image in Header component for accessibility

--- routing/src/Common/Header.js
+++ routing/src/Common/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 const Header = () => {
     return(
         <header className="header">
-        <img src="https://img.freepik.com/premium-vector/ph-logo-design_731343-380.jpg"></img>
+        <img src="https://img.freepik.com/premium-vector/ph-logo-design_731343-380.jpg" alt="logo"></img>
         <div className="links">
           <NavLink to={"/"}>Home</NavLink>
           <NavLink to={"/about"}>About</NavLink>
