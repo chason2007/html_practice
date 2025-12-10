91690e8301bc7c76370ed318ee8fc81507f039b0
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-10 09:47:40 +0530

    the counter will reset to 0 when value is negative

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -13,22 +13,32 @@ function App() {
     setVal(val-1);
   };
 
-  // side effects
-  //Args: 1. Callback fn 2. Dependency array(props or state)
-  // 1. with empty dependency array -> runs once after initial render
-  // 2. with specific state/props -> runs when that specific state/props changes
-  // 3. with no dependency array -> runs after every render with dependency array
   useEffect(() => {
-    console.log("UseEffect - run once after initial render");
-  }, []);
-  
-  useEffect(() => {
-    console.log("UseEffect - with array of depensencies");
+    if(val<0){
+      setTimeout(() => {
+        setVal(0);
+      }, 2000);
+    }
   }, [val]);
 
-  useEffect(() => {
-    console.log("UseEffect - without array");
-  });
+
+
+  // // side effects
+  // //Args: 1. Callback fn 2. Dependency array(props or state)
+  // // 1. with empty dependency array -> runs once after initial render
+  // // 2. with specific state/props -> runs when that specific state/props changes
+  // // 3. with no dependency array -> runs after every render with dependency array
+  // useEffect(() => {
+  //   console.log("UseEffect - run once after initial render");
+  // }, []);
+  
+  // useEffect(() => {
+  //   console.log("UseEffect - with array of depensencies");
+  // }, [val]);
+
+  // useEffect(() => {
+  //   console.log("UseEffect - without array");
+  // });
   return (
     <div className="App">
       <Counter 
