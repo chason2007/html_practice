dd65811cb67041505bf358d0a35bd2ae12ec3791
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-10 09:38:44 +0530

    useEffect and its types of use cases

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Counter from "./Counter";
 
 function App() {
@@ -13,6 +13,22 @@ function App() {
     setVal(val-1);
   };
 
+  // side effects
+  //Args: 1. Callback fn 2. Dependency array(props or state)
+  // 1. with empty dependency array -> runs once after initial render
+  // 2. with specific state/props -> runs when that specific state/props changes
+  // 3. with no dependency array -> runs after every render with dependency array
+  useEffect(() => {
+    console.log("UseEffect - run once after initial render");
+  }, []);
+  
+  useEffect(() => {
+    console.log("UseEffect - with array of depensencies");
+  }, [val]);
+
+  useEffect(() => {
+    console.log("UseEffect - without array");
+  });
   return (
     <div className="App">
       <Counter 
