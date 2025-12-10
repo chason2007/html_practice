1afd60be9563da02d1667e3de73c1a668e636bc5
chason2007 <chasonchevyhurtis.2024cse@sece.ac.in>
2025-12-10 12:00:31 +0530

    update App component to use Outlet for rendering child routes

--- routing/src/App.js
+++ routing/src/App.js
@@ -1,10 +1,10 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
-import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      RouterProvider
+      <Outlet></Outlet>
     </div>
   );
 }
